5ee5726aaa69fc899a477e8ad4d07f0d34d890ee
mikki-unix <mikki.anjos@sptech.school>
2025-07-03 10:30:21 -0300

    Início do projeto My Travel Journal
    Organizei a estrutura do projeto, incluindo os componentes no src.
    
    Também incluí um .gitignore no repositório para ignorar todos os node_modules de todos os futuros projetos; e mudei o README, tendo em mente que não vou mais fazer os exercícios via a plataforma da Scrimba.

---
+++ my-travel-journal/src/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <header>
+      <img src="./src/assets/globe.png" alt="Globe icon"/>
+      <h1>my travel journal.</h1>
+    </header>
+  );
+} 
