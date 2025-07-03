aae48e3225716ee2bf1cd418acabc2c926ec29e7
mikki-unix <mikki.anjos@sptech.school>
2025-07-03 13:50:01 -0300

    Contato de gatos!
    Projeto retirado da [oitava aula da segunda seção da Scrimba](https://scrimba.com/learn-react-c0e/~0ea)
    
    O objetivo dos exercícios com esse projeto é  aprender o uso de props.
    Embora nesse primeiro commit há a presença desta feature, a intenção construir um programa "quebrado" para entender o porquê ela é tão útil.
    
    Os próximos commits implementarão, pouco a pouco, os Props, seguindo os exercícios da Scrimba.

---
+++ contacts/src/App.jsx
@@ -0,0 +1,14 @@
+import Contact from "./Contact.jsx"
+
+function App() {
+  return (
+    <div className="contacts">
+      <Contact/>
+      <Contact/>
+      <Contact/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
