cc53315b30d7b26c91f99c7fecda5292609264af
mikki-unix <mikki.anjos@sptech.school>
2025-07-06 12:27:55 -0300

    Mudança no estilo do Header
    Agora o container absolue é na verdade a div.banner.
    Através desse comportamento posso definir um tamanho pro header, e o resto dos elementos do body vão respeitar esse tzmanho, não ficando por baixo dele, como acontencia anteriormente.

--- melhores-piadas-r-tiodopave/src/App.jsx
+++ melhores-piadas-r-tiodopave/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header.jsx";
+import Post from "./Post.jsx";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Post />
     </>
   );
 }
--- melhores-piadas-r-tiodopave/src/Post.jsx
+++ melhores-piadas-r-tiodopave/src/Post.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./Post.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function Post() {
+  return (
+    <h1>opa</h1>
+  );
+}
