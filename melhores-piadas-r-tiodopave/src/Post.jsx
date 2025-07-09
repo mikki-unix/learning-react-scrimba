fe7c416aaea354975a0cb252ad6b8a79471f0454
mikki-unix <mikki.anjos@sptech.school>
2025-07-09 15:44:23 -0300

    Esqueleto do post

--- melhores-piadas-r-tiodopave/src/Post.jsx
+++ melhores-piadas-r-tiodopave/src/Post.jsx
@@ -1,7 +1,33 @@
 import "./Post.css";
 
+import upvote from "./assets/upvote.svg";
+import downvote from "./assets/downvote.svg";
+import comment from "./assets/comment.svg";
+
 export default function Post() {
   return (
-    <h1>opa</h1>
+    <section>
+      <div className="usuario">
+        <img src="" alt="Foto de usuario" />
+        <strong>u/usuario</strong>
+        <span>há 0 anos</span>
+      </div>
+
+      <h1>Setup</h1>
+      <p>Puch-line com spoiler</p>
+
+      <section className="classificacao">
+        <div>
+          <img src={upvote} alt="Ícone de upvote" />
+          <span>0</span>
+          <img src={downvote} alt="Ícone de downvote" />
+        </div>
+
+        <div>
+          <img src={comment} alt="Ícone de balão de fala" />
+          <span>0</span>
+        </div>
+      </section>
+    </section>
   );
 }
