fea72c97ef841f0ddfb6ade99e940827bded11e8
mikki-unix <mikki.anjos@sptech.school>
2025-08-07 14:36:11 -0300

    Event listener no Form

--- chef-claude/src/Main.jsx
+++ chef-claude/src/Main.jsx
@@ -1,14 +1,29 @@
-import "./Main.css"
+import "./Main.css";
 
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const ingredientListItems = ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const newIngredient = formData.get("ingredient");
+
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+  }
+
   return (
     <main>
-      <form>
-        <input placeholder="e.g. oregano"/>
-        <button>
-          Add ingredient
-        </button>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="e.g. oregano" name="ingredient"/>
+        <button>Add ingredient</button>
       </form>
+
+      <ul>{ingredientListItems}</ul>
     </main>
-  )
+  );
 }
